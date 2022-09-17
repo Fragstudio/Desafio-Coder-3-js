822761f3c400c0e199cc838bbfa150549bfcd385
Franco Aguilo <developer.frag@gmail.com>
2022-09-17 16:42:46 -0300

    Correccion de trabajo

--- js/main.js
+++ js/main.js
@@ -1,69 +1,83 @@
 let listaDeConsultas = [];
-let finDeCiclo = prompt("Ingrese ESC para salir");
+let productos = [];
+let cuotas = [];
 let producto = parseInt(prompt("cantidad de producto deseado"));
-let cuotas = parseInt(prompt("cantidad de cuotas deseadas, 1, 3, 6 o 12"));
-finDeCiclo = finDeCiclo.toUpperCase();
+let cuota = parseInt(prompt("cantidad de cuotas deseadas, 1, 3, 6 o 12"));
+let finDeCiclo = prompt("Ingrese ESC para salir").toUpperCase();
 //$100 es el valor estatico del producto en el cual baso el simulador
 const valorCuota = (a, b) => 100 * a + 100 * a * (b / 100);
 const datoValido = (a, b) => (a > 0 && b == 1) || b == 3 || b == 6 || b == 12;
-const datoInvalido = (a, b) => datoValido(a,b) == false;
+const datoInvalido = (a, b) => datoValido(a, b) == false;
 const fin = (a) => a != "ESC";
 
 do {
   if (fin(finDeCiclo)) {
-    while (datoValido(producto, cuotas) && fin(finDeCiclo)) {
+    while (datoValido(producto, cuota) && fin(finDeCiclo)) {
       let valor = 0;
-      switch (cuotas) {
+      switch (cuota) {
         case 1:
-          valor = valorCuota(producto, cuotas);
+          valor = valorCuota(producto, cuota);
           listaDeConsultas.push(valor);
+          productos.push(producto);
+          cuotas.push(cuota);
           alert("El valor de la cuota es: " + valor);
           break;
         case 3:
           valor = valorCuota(producto, cuotas);
           listaDeConsultas.push(valor);
+          productos.push(producto);
+          cuotas.push(cuota);
           alert("El valor de la cuota es: " + valor);
           break;
         case 6:
-          valor = valorCuota(producto, cuotas);
+          valor = valorCuota(producto, cuota);
           listaDeConsultas.push(valor);
+          productos.push(producto);
+          cuotas.push(cuota);
           alert("El valor de la cuota es: " + valor);
           break;
         case 12:
-          valor = valorCuota(producto, cuotas);
+          valor = valorCuota(producto, cuota);
           listaDeConsultas.push(valor);
+          productos.push(producto);
+          cuotas.push(cuota);
           alert("El valor de la cuota es: " + valor);
           break;
       }
       producto = parseInt(prompt("cantidad de producto deseado"));
-      cuotas = parseInt(prompt("cantidad de cuotas deseadas, 1, 3, 6 o 12"));
-      finDeCiclo = prompt("Ingrese ESC para salir");
-      finDeCiclo = finDeCiclo.toUpperCase();
+      cuota = parseInt(prompt("cantidad de cuotas deseadas, 1, 3, 6 o 12"));
+      finDeCiclo = prompt("Ingrese ESC para salir").toUpperCase();
     }
   } else {
     break;
   }
-  while (datoInvalido(producto, cuotas)) {
+  while (datoInvalido(producto, cuota)) {
     alert(
       "los datos ingresados son incorrectos, por favor, vuelva a ingresar los datos"
     );
     producto = parseInt(prompt("cantidad de producto deseado"));
-    cuotas = parseInt(prompt("cantidad de cuotas deseadas, 1, 3, 6 o 12"));
+    cuota = parseInt(prompt("cantidad de cuotas deseadas, 1, 3, 6 o 12"));
   }
-} while (datoValido(producto, cuotas) && fin(finDeCiclo));
+} while (datoValido(producto, cuota) && fin(finDeCiclo));
 
 let consulta = prompt(
-  "Desea ver alguna consulta ya realizada? Ingrese si o no"
-);
-consulta = consulta.toUpperCase();
-do {
-  let numDeConsulta = parseInt(prompt("Ingrese el numero de consulta"));
-  if (numDeConsulta > 0 && numDeConsulta <= listaDeConsultas.length) {
-    numDeConsulta = numDeConsulta - 1;
-    alert("La consulta fue " + listaDeConsultas[consulta]);
-  } else {
-    alert("El numero de caso es incorrecto");
-  }
-  consulta = prompt("Desea ver alguna consulta ya realizada? Ingrese si o no");
-  consulta = consulta.toUpperCase();
-} while (consulta == "SI");
+  "Desea revisar el detalle de las consultas realizadas? Ingrese si o no"
+).toUpperCase();
+
+do{
+    alert('las consultas realizadas fueron las sigiuientes: ' + listaDeConsultas);
+    let opcion = parseInt(prompt('Ingrese la consulta de la cual desea ver el detalle'));
+    if (opcion <= listaDeConsultas.length && opcion >0){
+        alert('Detalle: ' + " " +
+        'Cantidad de producto: ' + productos[opcion-1] + ' ' +
+        'Cantidad de cuotas: ' + cuotas[opcion-1] + ' ' +
+        'Valor total: ' + listaDeConsultas[opcion-1]);
+    }else {
+        alert('la opcion ingresada es incorrecta, vuelva a intentar')
+        opcion = parseInt(prompt('Ingrese la consulta de la cual desea ver el detalle'));
+    }
+    consulta = prompt(
+        "Desea revisar el detalle de las consultas realizadas? Ingrese si o no"
+      ).toUpperCase();
+
+}while(consulta == 'SI');
